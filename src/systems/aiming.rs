63f3c8a1f007127891ffa26c707d5771dc8f5ccb
Tom Smallridge <tom@evari.insure>
2019-11-12 17:19:08 +0800

    more wip, trying to use some/ok to appease borrow checker

--- src/systems/aiming.rs
+++ src/systems/aiming.rs
@@ -11,6 +11,11 @@ use amethyst::{
 
 use crate::components::Reticle;
 
+struct ReticleToUpdate {
+  new_position: Vector2<f32>,
+  transform: Transform
+}
+
 pub struct AimingSystem;
 
 impl<'s> System<'s> for AimingSystem {
@@ -27,30 +32,48 @@ impl<'s> System<'s> for AimingSystem {
     &mut self,
     (reticles, mut transforms, cameras, active_camera, screen, input): Self::SystemData,
   ) {
-      // TODO: Implement this aiming system:
-      // - get mouse position - done
-      // - get world coords of mouse - done
-      // - draw reticle at that position - wip
-      // - Can we colour the reticle based on player/game state maybe?? - not started
-
-      println!("We're aiming!! " ); // TODO: delete debug line
+    // TODO: Implement this aiming system:
+    // - get mouse position - done
+    // - get world coords of mouse - done
+    // - draw reticle at that position - wip
+    // - Can we colour the reticle based on player/game state maybe?? - not started
+    
+    // This system operates under the assumption there is a single reticle. Could use a list here if that's insufficient
+    let mut reticle_to_update: Some(ReticleToUpdate) = None;
+    
     for (_reticle, _transform) in (&reticles, &mut transforms).join() {
       if let Some((screen_x, screen_y)) = input.mouse_position() {
         if let Some(camera_entity) = active_camera.entity {
           let camera = cameras.get(camera_entity).unwrap();
-          let camera_transform = transforms.get_mut(camera_entity).unwrap();
+          let camera_transform = transforms.get(camera_entity).unwrap();
           let world_position = camera.projection().screen_to_world_point(
             Point3::new(screen_x, screen_y, 0.0),
             Vector2::new(screen.width(), screen.height()),
             camera_transform,
           );
 
-          // Move reticle to our mouse position
-          _transform.set_translation_xyz(world_position.x, world_position.y, 0.0);
+        // Mark reticle for position update
+        reticle_to_update = Some(ReticleToUpdate {
+          transform: _transform,
+          new_position: Vector2::new(world_position.x, world_position.y)
+        } _transform);
+          // _transform.set_translation_xyz(world_position.x, world_position.y, 0.0);
         }
       }
-
-
     }
+
+    // Perform our update on reticle position
+    match reticles_to_update {
+      Some(reticle) => {
+        reticle.transform.set_translation_xyz(reticle.new_position.x, reticle.new_position.y, 0.0);
+
+      },
+      _ => {}
+    };
+
+  //   deletions.push(Deletion {
+  //     hook: _entity,
+  //     player: _hook.owner,
+  // });
   }
 }
